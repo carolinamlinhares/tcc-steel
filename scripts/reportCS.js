70353bf4d4ff4302bb4a19a4d1568a7f141b490a
Carolina Linhares <carolinamlinhares@gmail.com>
2016-12-26 22:54:40 -0300

    Update reportCS.js

--- scripts/reportCS.js
+++ scripts/reportCS.js
@@ -34,7 +34,7 @@ var resultado = [];
 
 resultado = JSON.parse(getJsonFromUrl().test);
 
-var text1 = "Como " + resultado.lambA + " = " + \(c=\frac{2}{3x}+\ \mathop{\int\!\!\!\!\int}{xdx}\) + " = " + resultado.lambpA
+var text1 = "Como " + resultado.lambA + " = " + "<span> \(c=\frac{2}{3x}+\ \mathop{\int\!\!\!\!\int}{xdx}\) </span>" + " = " + resultado.lambpA
 
 
 $(document).ready(function() {
