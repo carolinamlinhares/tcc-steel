0e7b49c7e1b75ac5c4eabe7a11ecd2ce4bb08363
Carolina Linhares <carolinamlinhares@gmail.com>
2017-01-08 18:50:31 -0300

    Update reportDS.js

--- scripts/reportDS.js
+++ scripts/reportDS.js
@@ -83,7 +83,7 @@ $(document).ready(function() {
     
     $('#suggestion_table').append('<table></table>');
     var table = $('#suggestion_table').children();
-        i;
+    var i;
     table.append('<tr><th> Perfil </th> <th> Cortante \(V_{rd}\) (KN) </th> <th> Momento \(M_{rd}\) (KN.m) </th> <th> Ratio Cortante </th> <th> Ratio Momento </th> </tr>');
     for (i = 0; i < resultado.suggestion.length; i += 1) {
         table.append('<tr><td>' +  resultado.suggestion[i].perfil.bitola + '</td>' + '<td>' +  resultado.suggestion[i].vrd + '</td>' + '<td>' +  resultado.suggestion[i].mrdOut + '</td>' + '<td>' +  resultado.suggestion[i].ratioDSV + '</td>' + '<td>' +  resultado.suggestion[i].ratioDSM + '</td>' + '</tr>');
