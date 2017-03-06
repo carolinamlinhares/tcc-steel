026517fcfc6cbc0b61acb7aeffa90938e13a815b
julianaliborio <juliana.liboriolima@gmail.com>
2017-03-06 09:19:50 -0300

    Update scriptCC.js

--- scripts/scriptCC.js
+++ scripts/scriptCC.js
@@ -255,10 +255,6 @@ function processFormCC() {
         situationCG = "Incoerente";
     }
     
-    if (situationCG === "Incoerente") {
-        alert("A altura útil não está coerente com a altura da viga, cobrimento e bitolas utilizadas. Por favor, verifique os dados de entrada.");
-    }
-    
     console.log("situationCG = " + situationCG);
       
     // Checking Dominio
@@ -295,22 +291,29 @@ function processFormCC() {
     
     // Results
     msd = mk * gamaf;
-    if (msd <= md && situationD === "Aprovado" && situationLN === "Aprovada") {
-        result = "A viga resiste ao momento fletor solicitado. Verifique o relatório  completo para obter o momento máximo que a viga resiste e garantir que a mesma seja econômica";
-    } else if (msd <= md && situationD === "Aprovado" && situationLN === "Reprovada") {
-        result = "A viga resiste ao momento fletor, mas não atende ao limite da linha neutra estabelecido em norma. Sugere-se aumentar a altura útil.";
-    } else {
-        result = "A viga não resiste ao momento fletor solicitado. Sugere-se aumentar a seção transversal ou a resistência do concreto. Verifique o relatório para mais opções.";
-    }
-    alert(result);
     
-    if (h > 60) {
-        armPele = 0.001 * bw * h;
-        resultArmPele = "É necessário utilizar armadura de pele com " + armPele + "cm² por face";
+    if (situationCG === "Incoerente") {
+        alert("A altura útil não está coerente com a altura da viga, cobrimento e bitolas utilizadas. Por favor, verifique os dados de entrada.");
     } else {
-        resultArmPele = "Não é necessário utilizar armadura de pele";
+    
+        if (msd <= md && situationD === "Aprovado" && situationLN === "Aprovada") {
+            result = "A viga resiste ao momento fletor solicitado. Verifique o relatório  completo para obter o momento máximo que a viga resiste e garantir que a mesma seja econômica";
+        } else if (msd <= md && situationD === "Aprovado" && situationLN === "Reprovada") {
+            result = "A viga resiste ao momento fletor, mas não atende ao limite da linha neutra estabelecido em norma. Sugere-se aumentar a altura útil.";
+        } else {
+            result = "A viga não resiste ao momento fletor solicitado. Sugere-se aumentar a seção transversal ou a resistência do concreto. Verifique o relatório para mais opções.";
+        }
+        alert(result);
+    
+        if (h > 60) {
+            armPele = 0.001 * bw * h;
+            resultArmPele = "É necessário utilizar armadura de pele com " + armPele + "cm² por face";
+        } else {
+            resultArmPele = "Não é necessário utilizar armadura de pele";
+        }
+        alert(resultArmPele);
+        
     }
-    alert(resultArmPele);
 }
 
     
